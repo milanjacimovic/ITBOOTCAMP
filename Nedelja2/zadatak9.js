ef36e636a0da15ad63f22c3a5417caf2d6a00609
milanjacimovic <60388959+milanjacimovic@users.noreply.github.com>
2020-02-06 10:44:31 +0100

    Add files via upload

--- Nedelja2/zadatak9.js
+++ Nedelja2/zadatak9.js
@@ -1,8 +1,7 @@
+
 let x = ''
 let n = 5
-x+= ' '.repeat(n-1) + '#'.repeat(n-4) + ' ' + '#'.repeat(n-4) + ' ' + '\n'
-x+= ' '.repeat(n-2) + '#'.repeat(n-3) + ' ' + '#'.repeat(n-3) + ' ' + '\n'
-x+= ' '.repeat(n-3) + '#'.repeat(n-2) + ' ' + '#'.repeat(n-2) + ' ' + '\n'
-x+= ' '.repeat(n-4) + '#'.repeat(n-1) + ' ' + '#'.repeat(n-1) + ' ' + '\n'
-x+= ' '.repeat(n-5) + '#'.repeat(n) + ' ' + '#'.repeat(n) + ' ' + '\n'
+for (n=4; n>-1; n--) {
+x += ' '.repeat(n) + '#'.repeat(5-n) + ' ' + '#'.repeat(5-n) + '\n'
+}
 console.log(x)
