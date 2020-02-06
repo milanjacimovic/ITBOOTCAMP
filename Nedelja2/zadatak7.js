5eaf5ebbfebac4a3163012aa02fada174cc89700
milanjacimovic <60388959+milanjacimovic@users.noreply.github.com>
2020-02-06 10:16:25 +0100

    Add files via upload

---
+++ Nedelja2/zadatak7.js
@@ -0,0 +1,6 @@
+let x = ''
+let n = 5
+for (n=1; n<6; n++) {
+x += '#'.repeat(n) + '\n'
+}
+console.log(x)
---
+++ Nedelja2/zadatak8.js
@@ -0,0 +1,7 @@
+
+let x = ''
+let n = 5
+for (n=4; n>-1; n--) {
+x += ' '.repeat(n) + '#'.repeat(5-n) + '\n'
+}
+console.log(x)
