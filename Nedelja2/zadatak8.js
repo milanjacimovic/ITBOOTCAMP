dcdc398dacbaf7e2c37a11065af4c19a583bee6a
milanjacimovic <60388959+milanjacimovic@users.noreply.github.com>
2020-02-06 10:15:51 +0100

    Delete zadatak8.js

--- Nedelja2/zadatak8.js
+++
@@ -1,9 +0,0 @@
-let x = ''
-let n = 5
-x += ' '.repeat(n-1) + '#'.repeat(n-4) + '\n'
-x += ' '.repeat(n-2) + '#'.repeat(n-3) + '\n'
-x += ' '.repeat(n-3) + '#'.repeat(n-2) + '\n'
-x += ' '.repeat(n-4) + '#'.repeat(n-1) + '\n'
-x += ' '.repeat(n-5) + '#'.repeat(n) + '\n'
-
-console.log(x)
