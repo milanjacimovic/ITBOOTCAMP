428a32c9b3965c8c9731db4860805f6676f0a6e3
milanjacimovic <60388959+milanjacimovic@users.noreply.github.com>
2020-02-07 09:37:00 +0100

    Add files via upload

--- petak2.js
+++ petak2.js
@@ -1,6 +1,6 @@
 let x = ''
 let n = 5
-for (i=0; i<=n; i++) {
-x += ' '.repeat(i) + '#'.repeat(n-i) + ' ' + '#'.repeat(n-i) + '\n'
+for (i=1; i<=n; i++) {
+x += ' '.repeat(n-i) + '#'.repeat(i) + ' ' + '#'.repeat(i) + '\n'
 }
 console.log(x)
