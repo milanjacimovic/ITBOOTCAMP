c72f436d8158984a09a5b4a3a50ffdba4698a945
milanjacimovic <60388959+milanjacimovic@users.noreply.github.com>
2020-03-08 19:54:24 +0100

    Update launch.js

--- Nedelja6/src/components/launch.js
+++ Nedelja6/src/components/launch.js
@@ -8,6 +8,7 @@ const Launch = (launch) => {
     } = launch;
     const div = document.createElement('div');
     div.className = 'launch';
+
     const inner_divs = []
     // Inner div 0 je za sliku
     // Inner div 1 je za Naziv
@@ -19,18 +20,21 @@ const Launch = (launch) => {
         div.appendChild(innerDivx);
     }
     //Slika
-    const img = document.createElement('img');
-    img.src = mission_patch;
+    const img=document.createElement('img');
+    img.src=mission_patch;
     inner_divs[0].appendChild(img);
+
     // Naziv misije
-    inner_divs[1].innerHTML = `<span>${mission_name}</span>`
+    inner_divs[1].innerHTML=`<span>${mission_name}</span>`
+
     //Datum i godina
-    let date = new Date(launch_date_utc);
-    const options = { weekday: 'short', month: 'numeric', day: 'numeric' }
+    let date= new Date(launch_date_utc);
+    const options={weekday:'short',month:'numeric',day:'numeric'}
     // date.toLocaleDateString()
-    inner_divs[2].innerHTML =
-        `<span>${launch_year}</span><span>${date.toLocaleDateString('sr-RS', options)}</span>`;
+    inner_divs[2].innerHTML=
+    `<span>${launch_year}</span><span>${date.toLocaleDateString('sr-RS',options)}</span>`;
+
     return div;
 }
 
-export  {Launch};
+export default Launch;
