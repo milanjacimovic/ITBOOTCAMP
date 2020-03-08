6f13e1760373e189502ca20142ea4e3e9b9e6cbd
milanjacimovic <60388959+milanjacimovic@users.noreply.github.com>
2020-03-08 19:54:50 +0100

    Update launch_list.js

--- Nedelja6/src/components/launch_list.js
+++ Nedelja6/src/components/launch_list.js
@@ -1,29 +1,35 @@
 import { getPastLaunches, getInfo } from '../services/spacex_service'
-import { Launch} from './launch';
+import Launch from './launch';
 //Add filters
 const LaunchList = () => {
     const div = document.createElement('div');
     div.className = 'launch-list';
+
     getPastLaunches().then(response => {
         // let data=response.data;
         let { data } = response;
+        console.log(response)
+
         // const launch=Launch()
         data.forEach(element => {
             div.appendChild(Launch(element));
         });
+
     }, error => { console.log(error) });
+
     return div;
 }
-const InfoHed = ()=>{
+
+const InfoHed = () => {
     const div = document.createElement('div');
     div.className = 'headerInfo';
     getInfo().then(response => {
         let { data } = response;
-        let p=document.createElement('p')
-        p.innerHTML=`${data.name}`
+        let p = document.createElement('p')
+        p.innerHTML = `${data.name}`
         div.append(p)
-       
+
     }, error => { console.log(error) });
     return div;
 }
-export  {LaunchList, InfoHed};
+export { LaunchList, InfoHed }
