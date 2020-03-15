3992db358377221a23815abf4880b2eb9ad0f1fe
milanjacimovic <60388959+milanjacimovic@users.noreply.github.com>
2020-03-15 15:15:48 +0100

    Add files via upload

---
+++ Nedelja7/Zadatak1/Preko funkcija/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Form from './Form';
+
+function App() {
+  return (
+   <Form string='Klikni'></Form>
+  );
+}
+
+export default App;
