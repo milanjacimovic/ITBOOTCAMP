949ca2c34590bf6892b4538dc0e81522902dfcb2
milanjacimovic <60388959+milanjacimovic@users.noreply.github.com>
2020-03-23 12:42:18 +0100

    Update Select.js

--- Test React/src/Components/Select.js
+++ Test React/src/Components/Select.js
@@ -5,7 +5,7 @@ const Select = ({handleChange}) => {
         <div>
             <h3>Izaberi koliko zaposlenih da prikazem:</h3>
          <select onChange={(e) => handleChange(e)}>
-            <option value={'All'}>All employees</option>
+            <option value={'All'}>Svi zaposleni</option>
             <option value={'5'}>5</option>
             <option value={'10'}>10</option>
             <option value={'20'}>20</option>
